fa948ae3b610f90db3f8d7bab8a68a94d50544d6
PavanKumar-bollam <bollampavan2909@gmail.com>
2023-12-28 06:26:45 +0000

    solution

---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,50 @@
+// Write your code here
+import Popup from "reactjs-popup";
+
+import CartContext from "../../context/CartContext";
+import Payment from "../Payment";
+
+import "./index.css";
+import "reactjs-popup/dist/index.css";
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {(value) => {
+      const { cartList } = value;
+      const cartListLength = cartList.length;
+      let total = 0;
+      cartList.forEach((eachCartItem) => {
+        total += eachCartItem.price * eachCartItem.quantity;
+      });
+
+      return (
+        <>
+          <div className="cart-summary-container">
+            <h1 className="order-total-value">
+              <span className="order-total-label">Order Total:</span>Rs {total}{" "}
+              /-
+            </h1>
+            <p className="total-items">{cartListLength} Items in cart</p>
+            <button type="button" className="checkout-button d-lg-none">
+              Checkout
+            </button>
+            <Popup
+              modal
+              trigger={
+                <button
+                  type="button"
+                  className="trigger-button checkout-button d-sm-none">
+                  Checkout
+                </button>
+              }>
+              <h1>Rs {total} /-</h1>
+              <Payment />
+            </Popup>
+          </div>
+        </>
+      );
+    }}
+  </CartContext.Consumer>
+);
+
+export default CartSummary;
